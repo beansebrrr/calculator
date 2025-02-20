9e0a3483af0bfa28580b2c7aecb1ea71f19a8d71
beansebrrr <173890355+beansebrrr@users.noreply.github.com>
2025-02-20 10:34:23 +0400

    ✨ Made functions for basic math operations

---
+++ script.js
@@ -0,0 +1,27 @@
+const add = (addend1, addend2) =>
+  addend1 + addend2;
+
+const subtract = (subtrahend, subtractor) =>
+  subtrahend - subtractor;
+
+const multiply = (multiplicand, multiplier) =>
+  multiplicand * multiplier;
+
+const divide = (dividend, divisor) =>
+  dividend / divisor;
+
+const operate = (num1, operator, num2) => {
+  switch (operator) {
+    case "+":
+      return add(num1, num2)
+    case "-":
+      return subtract(num1, num2)
+    case "*":
+      return multiply(num1, num2)
+    case "/":
+      return divide(num1, num2)
+  
+    default:
+      break;
+  }
+}
