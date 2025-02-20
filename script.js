31651e02bdfbaae554a8b84f689ba8febe47fb46
beansebrrr <173890355+beansebrrr@users.noreply.github.com>
2025-02-20 13:53:43 +0400

    🧠 Gave UI a brain (it's really bad)

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const multiply = (multiplicand, multiplier) =>
 const divide = (dividend, divisor) =>
   dividend / divisor;
 
-const operate = (num1, operator, num2) => {
+const operate = (num1, num2, operator) => {
   switch (operator) {
     case "+":
       return add(num1, num2);
@@ -29,3 +29,51 @@ const operate = (num1, operator, num2) => {
       break;
   }
 };
+
+
+/**
+ * UI
+ */
+
+const display = document.querySelector(".display")
+const btnSection = document.querySelector(".btnSection")
+let currentOperation = ''
+
+btnSection.addEventListener("click", (e) => {
+  const val = e.target.value
+  let output = '';
+  if (!val) {
+    return
+  } else if (val === "ac") {
+    currentOperation = '';
+    output = currentOperation;
+  }
+  else if (val === "=") {
+    output = getOutput(currentOperation);
+    currentOperation = '';
+  } else {
+    currentOperation = currentOperation.concat(val);
+    output = currentOperation;
+  }
+
+  // show input in display 
+  display.textContent = output;
+})
+
+function getOutput(operation) {
+  if (!validateSyntax(operation)) {
+    return "SYNTAX ERROR"
+  }
+
+  // split string
+  const nums = operation
+    .split(/[*\/+\-]{1}/)
+    .map(num => +num);
+  const [operator] = operation.match(/[*\/+\-]{1}/g);
+  return operate(...nums, operator)
+}
+
+function validateSyntax(str) {
+  const operationSyntax = /^\d+(?:\.\d*)?[*\/+\-]{1}\d+(?:\.\d*)?$/gm
+  return operationSyntax.test(str)
+}
