f43d1169f7909b0909123ea6441966ce853814cb
beansebrrr <173890355+beansebrrr@users.noreply.github.com>
2025-02-20 15:57:12 +0400

    ♻️ Revamp the brains (I'm proud of this one)

--- script.js
+++ script.js
@@ -37,3 +37,37 @@ const operate = (num1, num2, operator) => {
 
 const display = document.querySelector(".display")
 const btnSection = document.querySelector(".btnSection")
+
+btnSection.addEventListener("click", btn => {
+  btn = btn.target;
+  if (btn.id === "clear") {
+    display.value = '';
+  } else if (btn.id === "evaluate") {
+    const output = evaluate(display.value);
+    display.value = output;
+  };
+
+  const btnType = btn.className;
+  const btnValue = btn.textContent;
+
+  if (btnType.includes("numBtn") || btnType.includes("operatorBtn")) {
+    display.value += btnValue;
+  };
+});
+
+function evaluate(expression) {
+  let output
+  const operands = expression
+    .split(/[+\-/*]/)
+    .map(operand => +operand);
+  const operators = expression
+    .split("")
+    .filter(char => char.match(/[+\-/*]/));
+  
+  while (operators.length > 0) {
+    let operator = operators.shift()
+    output = operate(...operands.splice(0,2), operator);
+    operands.unshift(output);
+  }
+  return output;
+};
